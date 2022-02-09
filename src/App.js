9b4e0cbeb07cc65a6d13242415395474d2178e3b
Kevin Minh <kevinminh.ng@gmail.com>
2022-02-09 16:54:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Article from "./components/article/Article";
+import Brand from "./components/brand/Brand";
+import Cta from "./components/cta/Cta";
+import Feature from "./components/feature/Feature";
+import Navbar from "./components/navbar/Navbar";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>GT-3</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
