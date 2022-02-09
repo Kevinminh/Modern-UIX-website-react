3497838a6d143b0cbb7138c5019daaa9f1baa6eb
Kevin Minh <kevinminh.ng@gmail.com>
2022-02-09 16:57:04 +0100

    First commit

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Brand } from "./brand/Brand";
 export { default as Cta } from "./cta/Cta";
 export { default as Feature } from "./feature/Feature";
 export { default as navbar } from "./navbar/Navbar";
+
